bb6bca56ad519227d61ee23e1404bd0a2f68f240
s28824 <s28824@pjwstk.edu.pl>
2024-03-11 09:29:45 +0100

    Modyfikacja 3

--- zad1/Program.cs
+++ zad1/Program.cs
@@ -1,6 +1,10 @@
 ﻿// See https://aka.ms/new-console-template for more information
 
-
 Console.WriteLine("Hello, Everybody!");
 
 int[] tbl = {1, 2, 3, 4};
+
+foreach (int num in tbl)
+{
+    Console.WriteLine(num);
+}
